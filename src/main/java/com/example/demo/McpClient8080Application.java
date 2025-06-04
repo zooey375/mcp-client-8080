922c763576fe3b176f74a5aa4f7a394bb96bc2a3
zooey <yu.ting375@gmail.com>
2025-06-04 11:47:09 +0800

    20250604_1147

---
+++ src/main/java/com/example/demo/McpClient8080Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McpClient8080Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McpClient8080Application.class, args);
+	}
+
+}
