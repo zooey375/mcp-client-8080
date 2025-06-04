ad76fc3848baf87e6487d4e230897892ee63290d
zooey <yu.ting375@gmail.com>
2025-06-04 12:25:31 +0800

    20250604_1225

--- src/main/java/com/example/demo/McpClient8080Application.java
+++ src/main/java/com/example/demo/McpClient8080Application.java
@@ -1,7 +1,11 @@
 package com.example.demo;
 
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.mcp.SyncMcpToolCallbackProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class McpClient8080Application {
@@ -10,4 +14,21 @@ public class McpClient8080Application {
 		SpringApplication.run(McpClient8080Application.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner runner(ChatClient.Builder chatClientBuilder, SyncMcpToolCallbackProvider toolCallbackProvider) {
+			return args -> {
+			
+				// 建立 ChatClient 並將 callback provider 註冊進去
+				ChatClient chatClient = chatClientBuilder.defaultTools(toolCallbackProvider).build();
+		
+				String prompt1 = "今天似乎運氣不錯想買5張樂透彩券，並幫我結帳";
+				System.out.println(prompt1);
+			
+			
+				String response1 = chatClient.prompt().user(prompt1).call().content();
+		
+				System.out.println("AI Agent 回應:" + response1);
+			};
+	}
+	
 }
